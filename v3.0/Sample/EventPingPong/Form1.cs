07654d0668eb574c64406213a39889c3acc13a98
SND\keithh_cp <SND\keithh_cp@4ef1bfeb-0a34-4d02-9305-240e5b07a168>
2012-11-16 22:24:26 +0000

    Branched from $/pubsub/v2.1
    git-svn-id: https://pubsub.svn.codeplex.com/svn@97356 4ef1bfeb-0a34-4d02-9305-240e5b07a168

---
+++ v3.0/Sample/EventPingPong/Form1.cs
@@ -0,0 +1,270 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.WebSolutionsPlatform.Event.PubSubManager;
+
+namespace Microsoft.Sample.EventPingPong
+{
+    public partial class Form1 : Form
+    {
+        private static object obj = new object();
+        private static Guid instanceId = Guid.NewGuid();
+        private static UInt64 counter;
+        private static DateTime startTime;
+        private static TimeSpan totalTime;
+        private static PublishEvent pubEvent;
+        private static SubscribeEvent subEvent;
+        private static PublishManager pubMgr;
+        private static SubscriptionManager subMgr;
+        private static SubscriptionManager.Callback subCallback;
+        private static SubscriptionManager.Callback pubCallback;
+
+        delegate void SetTextCallback(TextBox tb, string text);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pubEvent = new PublishEvent();
+            pubEvent.InstanceId = instanceId;
+
+            subEvent = new SubscribeEvent();
+
+            pubMgr = new PublishManager();
+
+            subCallback = new SubscriptionManager.Callback(SubscriptionCallback);
+            pubCallback = new SubscriptionManager.Callback(PublishCallback);
+
+            stopPubSubButton.Checked = true;
+            stopPubSubButton.Select();
+        }
+
+        private void stopPubSubButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (stopPubSubButton.Checked == true)
+            {
+                publishEventsButton.Checked = false;
+                subscribeEventsButton.Checked = false;
+
+                StopPubSub();
+            }
+        }
+
+        private void StopPubSub()
+        {
+            StopSubscribe();
+            StopPublish();
+        }
+
+        public void StartPublish()
+        {
+            startTime = DateTime.Now;
+            counter = 0;
+
+            timer1.Start();
+
+            pubEvent.EventNum = 0;
+
+            subMgr = new SubscriptionManager(1, pubCallback);
+            subMgr.ListenForEvents = true;
+            subMgr.AddSubscription(pubEvent.InstanceId, false);
+
+            while (true)
+            {
+                try
+                {
+                    pubMgr.Publish(pubEvent.Serialize());
+
+                    break;
+                }
+                catch
+                {
+                    Thread.Sleep(2);
+                }
+            }
+        }
+
+        public void StopPublish()
+        {
+            try
+            {
+                timer1.Stop();
+
+                if (subMgr != null)
+                {
+                    subMgr.RemoveSubscription(pubEvent.InstanceId);
+                    subMgr.ListenForEvents = false;
+                    subMgr = null;
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public void StartSubscribe()
+        {
+            startTime = DateTime.Now;
+            counter = 0;
+
+            timer1.Start();
+
+            subMgr = new SubscriptionManager(1, subCallback);
+            subMgr.ListenForEvents = true;
+            subMgr.AddSubscription(pubEvent.EventType, false);
+        }
+
+        public void StopSubscribe()
+        {
+            try
+            {
+                timer1.Stop();
+
+                if (subMgr != null)
+                {
+                    subMgr.RemoveSubscription(pubEvent.EventType);
+                    subMgr.ListenForEvents = false;
+                    subMgr = null;
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void publishEventsButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (publishEventsButton.Checked == true)
+            {
+                stopPubSubButton.Checked = false;
+                subscribeEventsButton.Checked = false;
+
+                StopPubSub();
+                StartPublish();
+            }
+        }
+
+        private void subscribeEventsButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (subscribeEventsButton.Checked == true)
+            {
+                stopPubSubButton.Checked = false;
+                publishEventsButton.Checked = false;
+
+                StopPubSub();
+                StartSubscribe();
+            }
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            StopPubSub();
+
+            Application.Exit();
+        }
+
+        private void SetTextbox(TextBox tb, string text)
+        {
+            try
+            {
+                if (this.InvokeRequired)
+                {
+                    SetTextCallback d = new SetTextCallback(SetTextbox);
+
+                    this.Invoke(d, new object[] { tb, text });
+                }
+                else
+                {
+                    tb.Text = text;
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                // This may happen when you Quit
+            }
+        }
+
+
+
+        public void SubscriptionCallback(Guid eventType, byte[] serializedEvent)
+        {
+            PublishEvent localEvent;
+
+            localEvent = new PublishEvent(serializedEvent);
+
+            Thread.Sleep(0);
+
+            lock (obj)
+            {
+                counter++;
+
+                subEvent.EventNum = localEvent.EventNum;
+                subEvent.InstanceId = localEvent.InstanceId;
+                subEvent.EventType = localEvent.InstanceId;
+
+                while (true)
+                {
+                    try
+                    {
+                        pubMgr.Publish(subEvent.Serialize());
+
+                        break;
+                    }
+                    catch
+                    {
+                        Thread.Sleep(2);
+                    }
+                }
+            }
+
+            SetTextbox(eventNumberSent, counter.ToString());
+        }
+
+        public void PublishCallback(Guid eventType, byte[] serializedEvent)
+        {
+            SubscribeEvent subscribeEvent;
+
+            subscribeEvent = new SubscribeEvent(serializedEvent);
+
+            Thread.Sleep(0);
+
+            if (subscribeEvent.InstanceId == instanceId)
+            {
+                lock (obj)
+                {
+                    counter++;
+
+                    pubEvent.EventNum++;
+
+                    while (true)
+                    {
+                        try
+                        {
+                            pubMgr.Publish(pubEvent.Serialize());
+
+                            break;
+                        }
+                        catch
+                        {
+                            Thread.Sleep(2);
+                        }
+                    }
+                }
+
+                SetTextbox(eventNumberSent, counter.ToString());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            totalTime = new TimeSpan(DateTime.Now.Ticks - startTime.Ticks);
+
+            SetTextbox(roundtripRate, ((double)(counter / totalTime.TotalSeconds)).ToString());
+        }
+    }
+}
---
+++ v3.0/Sample/WspEventLogTest/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Microsoft.WebSolutionsPlatform.Event;
+using Microsoft.WebSolutionsPlatform.Event.PubSubManager;
+
+namespace WspEventLogTest
+{
+    public class LogEntry : Event
+    {
+        public bool BooleanProp { get; set; }
+        public byte ByteProp { get; set; }
+        public byte[] ByteArrayProp { get; set; }
+        public char CharProp { get; set; }
+        public char[] CharArrayProp { get; set; }
+        public decimal DecimalProp { get; set; }
+        public double DoubleProp { get; set; }
+        public Int16 Int16Prop { get; set; }
+        public Int32 Int32Prop { get; set; }
+        public Int64 Int64Prop { get; set; }
+        public SByte SByteProp { get; set; }
+        public Single SingleProp { get; set; }
+        public String StringProp { get; set; }
+        public UInt16 UInt16Prop { get; set; }
+        public UInt32 UInt32Prop { get; set; }
+        public UInt64 UInt64Prop { get; set; }
+        public Version VersionProp { get; set; }
+        public DateTime DateTimeProp { get; set; }
+        public Guid GuidProp { get; set; }
+        public Uri UriProp { get; set; }
+        private static byte[] ipaddr = {192, 168, 0, 1};
+        public IPAddress IPAddressProp { get; set; }
+        public Dictionary<string, string> StringDictionaryProp { get; set; }
+        public Dictionary<string, object> ObjectDictionaryProp { get; set; }
+        public List<string> StringListProp { get; set; }
+        public List<object> ObjectListProp { get; set; }
+
+        public LogEntry(byte[] serializedEvent)
+            : base(serializedEvent)
+        {
+        }
+
+        public LogEntry()
+            : base()
+        {
+            this.EventType = new Guid("BAAA4109-BB83-46dc-AFA3-422527F209C7");
+
+            BooleanProp = true;
+            ByteProp = 7;
+            ByteArrayProp = new byte[] { 1, 2, 3, 4, 5 };
+            CharProp = 'a';
+            CharArrayProp = new char[] { 'a', 'b', 'c', 'd' };
+            DecimalProp = 123;
+            DoubleProp = 123.456;
+            Int16Prop = -128;
+            Int32Prop = -256;
+            Int64Prop = -512;
+            SByteProp = -8;
+            SingleProp = Single.MinValue;
+            StringProp = "stringprop value \" \\ / \b \f \n \r \t \\u1234 all done";
+            UInt16Prop = 128;
+            UInt32Prop = 256;
+            UInt64Prop = 512;
+            VersionProp = new Version("2.1.2.3");
+            DateTimeProp = DateTime.Now;
+            GuidProp = Guid.NewGuid();
+            UriProp = new Uri("http://localhost/default.htm");
+            IPAddressProp = new IPAddress(ipaddr);
+            StringDictionaryProp = new Dictionary<string, string>();
+            ObjectDictionaryProp = new Dictionary<string, object>();
+            StringListProp = new List<string>();
+            ObjectListProp = new List<object>();
+
+            StringDictionaryProp["DPkey1"] = "value 1";
+            StringDictionaryProp["DPkey2"] = "key2 value \" \\ / \b \f \n \r \t \\u1234 all done";
+            StringDictionaryProp["DPkey3 \" \\ / \b \f \n \r \t \\u1234 all done"] = "key3 value";
+
+            StringListProp.Add("string 1");
+            StringListProp.Add("string 2 \" \\ / \b \f \n \r \t \\u1234 all done");
+            StringListProp.Add("string 3");
+
+            ObjectDictionaryProp["KVkey1"] = StringProp;
+            ObjectDictionaryProp["KVkey2"] = DateTimeProp;
+            ObjectDictionaryProp["KVkey3"] = GuidProp;
+            ObjectDictionaryProp["KVkey4"] = StringDictionaryProp;
+            ObjectDictionaryProp["KVkey5"] = StringListProp;
+            ObjectDictionaryProp["KVkey6"] = Int32Prop;
+
+            ObjectListProp.Add(ObjectDictionaryProp);
+        }
+
+        public override void GetObjectData(WspBuffer buffer)
+        {
+            buffer.AddElement("BooleanProp", BooleanProp);
+            buffer.AddElement("ByteProp", ByteProp);
+            buffer.AddElement("ByteArrayProp", ByteArrayProp);
+            buffer.AddElement("CharProp", CharProp);
+            buffer.AddElement("CharArrayProp", CharArrayProp);
+            buffer.AddElement("StringListProp", StringListProp);
+            buffer.AddElement("ObjectListProp", ObjectListProp);
+            buffer.AddElement("DecimalProp", DecimalProp);
+            buffer.AddElement("DoubleProp", DoubleProp);
+            buffer.AddElement("Int16Prop", Int16Prop);
+            buffer.AddElement("Int32Prop", Int32Prop);
+            buffer.AddElement("Int64Prop", Int64Prop);
+            buffer.AddElement("SByteProp", SByteProp);
+            buffer.AddElement("SingleProp", SingleProp);
+            buffer.AddElement("StringProp", StringProp);
+            buffer.AddElement("StringDictionaryProp", StringDictionaryProp);
+            buffer.AddElement("UInt16Prop", UInt16Prop);
+            buffer.AddElement("UInt32Prop", UInt32Prop);
+            buffer.AddElement("UInt64Prop", UInt64Prop);
+            buffer.AddElement("ObjectDictionaryProp", ObjectDictionaryProp);
+            buffer.AddElement("VersionProp", VersionProp);
+            buffer.AddElement("DateTimeProp", DateTimeProp);
+            buffer.AddElement("GuidProp", GuidProp);
+            buffer.AddElement("UriProp", UriProp);
+            buffer.AddElement("IPAddressProp", IPAddressProp);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PublishManager pubMgr = new PublishManager();
+
+            LogEntry e1 = new LogEntry();
+            LogEntry e2;
+
+            byte[] eSerialized = e1.Serialize();
+
+            e2 = new LogEntry(eSerialized);
+
+            pubMgr.Publish(eSerialized);
+        }
+    }
+}
